52e299deeb3a67b7f5754f6283b60f0d140d1b00
Marko <kezual@users.noreply.github.com>
2017-04-10 22:24:18 +0200

    Add v2

---
+++ app/src/main/java/com/example/android/street_food/DisplayMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.street_food;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_menu);
+    }
+}
